9c5421397b10e5284cc29e886f11d74df241a61d
Bagus Tris Atmaja <b-atmaja@aist.go.jp>
2024-07-17 19:38:22 +0900

    Update run_ut_iemocap.sh

--- run_ut_iemocap.sh
+++ run_ut_iemocap.sh
@@ -4,7 +4,7 @@ exec > >(tee results_iemocap.txt) 2>&1
 run_threshold() {
     threshold=$1
     echo "Threshold: $threshold"
-    python3 -m nkululeko.ensemble cocosda_2024/exp_iemocap_hubert_svm.ini cocosda_2024/exp_iemocap_wavlm_svm.ini --method uncertainty_threshold --threshold $threshold | grep "UAR" | awk '{print $NF}'
+    python3 -m nkululeko.ensemble cocosda_2024/exp_iemocap_hubert_svm.ini cocosda_2024/exp_iemocap_wavlm_svm.ini cocosda_2024/exp_iemocap_audmodel_svm.ini --method uncertainty_threshold --threshold $threshold | grep "UAR" | awk '{print $NF}'
 }
 
 export -f run_threshold
