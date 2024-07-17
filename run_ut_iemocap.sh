b721855582915d147d43ca4e05e6787ec5ed5493
Bagus Tris Atmaja <b-atmaja@aist.go.jp>
2024-07-17 19:35:57 +0900

    Create run_ut_iemocap.sh

---
+++ run_ut_iemocap.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+exec > >(tee results_iemocap.txt) 2>&1
+
+run_threshold() {
+    threshold=$1
+    echo "Threshold: $threshold"
+    python3 -m nkululeko.ensemble cocosda_2024/exp_iemocap_hubert_svm.ini cocosda_2024/exp_iemocap_wavlm_svm.ini --method uncertainty_threshold --threshold $threshold | grep "UAR" | awk '{print $NF}'
+}
+
+export -f run_threshold
+
+parallel run_threshold ::: $(seq 0.11 0.01 0.99)
+
+awk '/Threshold:/{t=$2} /uncertainty_threshold: UAR:/{if($5>max){max=$5; max_acc=$7; thresh=t}} END{print "Maximum UAR:", max, "Max ACC:", max_acc, "at threshold", thresh}' results_iemocap.txt
+
+
+
