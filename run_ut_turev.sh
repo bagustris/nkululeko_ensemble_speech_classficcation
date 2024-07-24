7e8e9cdc237ca37d437ef603370e37a17827d927
Bagus Tris Atmaja <b-atmaja@aist.go.jp>
2024-07-24 17:18:09 +0900

    Add files via upload

--- run_ut_turev.sh
+++ run_ut_turev.sh
@@ -4,11 +4,14 @@ exec > >(tee results_turev.txt) 2>&1
 run_threshold() {
     threshold=$1
     echo "Threshold: $threshold"
-    python3 -m nkululeko.ensemble exp_ococosda_2024/exp_turev_hubert.ini exp_ococosda_2024/exp_turev_audmodel.ini exp_ococosda_2024/exp_turev_wavlm.ini --method uncertainty_threshold --threshold $threshold | grep "UAR" | awk '{print $NF}'
+    python3 -m nkululeko.ensemble cocosda_2024/exp_turev_hubert.ini cocosda_2024/exp_turev_audmodel.ini --method uncertainty_threshold --threshold $threshold | grep "UAR" | awk '{print $NF}'
 }
 
 export -f run_threshold
 
-parallel run_threshold ::: $(seq 0.11 0.01 0.9)
+parallel run_threshold ::: $(seq 0.11 0.01 0.99)
 
 awk '/Threshold:/{t=$2} /uncertainty_threshold: UAR:/{if($5>max){max=$5; max_acc=$7; thresh=t}} END{print "Maximum UAR:", max, "Max ACC:", max_acc, "at threshold", thresh}' results_turev.txt
+
+
+
